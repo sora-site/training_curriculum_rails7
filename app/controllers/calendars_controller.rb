0712ba734f1ef189ceb3056eb6bcab7d6b3fd1da
sora-site <soshi.1999.13@gmail.com>
2024-05-27 22:06:08 +0900

    Issue6解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -21,29 +21,34 @@ class CalendarsController < ApplicationController
   def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
-    wday_num = Date.today.wday
-      if wday_num >= 7
-        wday_num = wday_num -7
-      end
-
+    
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
+    
     @week_days = []
-
+    
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+    
     7.times do |x|
       today_plans = []
+      wday_num = Date.today.wday
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       
-      days = {month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, :wday => wdays[wday_num+x.to_i]}
+      days = {month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, :wday => wdays[wday(wday_num+x.to_i)]}
       
       @week_days.push(days)
     end
+    
+  end
 
+  private
+  def wday(wday_num)
+    if wday_num >= 7
+      wday_num = wday_num -7
+    end
+    return wday_num
   end
 end
